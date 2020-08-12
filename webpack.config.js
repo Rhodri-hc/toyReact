8eccfbbe9d5a6f057bb19cc0cdc04f663a3308db
Rhodri-hc <13622450494@163.com>
2020-08-12 23:52:02 +0800

    安装jsx依赖

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,26 @@ module.exports = {
     mode: "development",
     optimization: {
         minimize: false
+    },
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                use:{
+                    loader:"babel-loader",
+                    options: {
+                        presets:["@babel/preset-env"],
+                        plugins:[
+                            [
+                                //配置react jsx语法 | 支持库：@babel/plugin-transform-react-jsx
+                                "@babel/plugin-transform-react-jsx",
+                                //自定义 createElement 调用对象名称，默认为React.createElement
+                                {pragma:"ToyReact.createElement"}
+                            ]
+                        ]
+                    }
+                }
+            }
+        ]
     }
 }
